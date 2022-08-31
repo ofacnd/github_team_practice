29608a5c2ff59ac839bbdd9283ac55fd88e0def0
Omer Fatih ORUCU <omerfatihorucu@gmail.com>
2022-08-31 10:28:02 -0600

    V1

---
+++ Desktop/github_team_practice/githubTeamPractice/src/GithubPractice.java
@@ -0,0 +1,44 @@
+public class GithubPractice {
+    public static void main(String[] args) {
+
+
+        //musa_bey 5-10
+
+
+
+
+
+        //basak_polat 11-16
+
+
+
+
+
+        //mehmet_fiskin 17-22
+
+
+
+
+
+        //asim_ates 23-28
+
+
+
+
+
+        //mustafa_msk 29-34
+
+
+
+
+
+        //yusuf_demir 35-40
+
+
+
+
+
+        //fatma_k 41-46
+
+    }
+}
